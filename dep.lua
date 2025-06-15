813cb91ce93505b1323a7c39ce3a44f613770d3b
Smecks899 <lonasweatfix@gmail.com>
2025-06-15 23:20:02 +0300

    Update dep.lua

--- dep.lua
+++ dep.lua
@@ -1,1 +1,65 @@
-# Libs
+local module = {}
+
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+
+local mouse = player:GetMouse()
+local camera = workspace.CurrentCamera
+
+
+function module.getClosestPlayer()
+    local maxDistance = 300 
+    local target 
+
+    for i,v in players:GetPlayers() do 
+        if v ~= player then 
+            local character = v.Character 
+            if character then   
+                local hrp = character:FindFirstChild("HumanoidRootPart") 
+
+                if hrp then 
+                    local distance = (hrp.Position - player.Character.HumanoidRootPart.Position).Magnitude 
+
+                    if distance < maxDistance then 
+                        maxDistance = distance
+                        target = character
+                    end 
+                end 
+            end 
+
+        end 
+    end 
+
+    return target
+
+end
+
+function module.getClosestToMouse()
+    local maxDistance = 90
+    local target  
+
+     for i,v in players:GetPlayers() do 
+        if v ~= player then 
+            local character = v.Character 
+            if character then   
+                local hrp = character:FindFirstChild("HumanoidRootPart") 
+
+                if hrp then 
+                    local vector,onScreen = camera:WorldToViewportPoint(hrp.Position)
+                    local distance = (Vector2.new(mouse.X,mouse.Y) - Vector2.new(vector.X,vector.Y)).Magnitude
+
+                    if onScreen and distance < maxDistance then 
+                        maxDistance = distance
+                        target = character
+                    end 
+ 
+                end 
+            end 
+
+        end 
+    end 
+
+    return target
+end
+
+return module 
